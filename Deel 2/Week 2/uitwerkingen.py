ca34b5e7d2bf92b5a7c9216290e9c81dbe446fc0
liz-cpu <40073038+liz-cpu@users.noreply.github.com>
2023-10-05 10:45:55 +0200

    Fix and finish part 2

--- Deel 2/Week 2/uitwerkingen.py
+++ Deel 2/Week 2/uitwerkingen.py
@@ -1,109 +1,153 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.sparse import csr_matrix
 
-# ==== OPGAVE 1 ====
-def plot_number(nrVector):
-    # Let op: de manier waarop de data is opgesteld vereist dat je gebruik maakt
-    # van de Fortran index-volgorde – de eerste index verandert het snelst, de 
-    # laatste index het langzaamst; als je dat niet doet, wordt het plaatje 
-    # gespiegeld en geroteerd. Zie de documentatie op 
-    # https://docs.scipy.org/doc/numpy/reference/generated/numpy.reshape.html
-    data = np.reshape(nrVector, (20, 20), order='F')
+
+def plot_number(nr_vector: np.ndarray) -> None:
+    """
+    Plots the given vector as a 20x20 matrix. Makes it pink just because.
+
+    :param nr_vector: the vector to plot
+    :return: None
+    """
+    data = np.reshape(nr_vector, (20, 20), order='F')
     plt.matshow(data, cmap='PuRd')
     plt.show()
     return None
 
-# ==== OPGAVE 2a ====
-def sigmoid(z):
-    # Maak de code die de sigmoid van de input z teruggeeft. Zorg er hierbij
-    # voor dat de code zowel werkt wanneer z een getal is als wanneer z een
-    # vector is.
-    # Maak gebruik van de methode exp() in NumPy.
+
+def sigmoid(z: np.matrix) -> np.matrix:
+    """
+    Computes the sigmoid of z. Uses the usual 1 / (1 + e^-z) formula.
+
+    :param z: the input
+
+    :return: the sigmoid of z
+    """
     return 1 / (1 + np.exp(-z))
 
 
-# ==== OPGAVE 2b ====
-def get_y_matrix(y, m):
-    # Gegeven een vector met waarden y_i van 1...x, retourneer een (ijle) matrix
-    # van m×x met een 1 op positie y_i en een 0 op de overige posities.
-    # Let op: de gegeven vector y is 1-based en de gevraagde matrix is 0-based,
-    # dus als y_i=1, dan moet regel i in de matrix [1,0,0, ... 0] zijn, als
-    # y_i=10, dan is regel i in de matrix [0,0,...1] (in dit geval is de breedte
-    # van de matrix 10 (0-9), maar de methode moet werken voor elke waarde van 
-    # y en m
-    y = y - 1
-    row = np.arange(m)
-    col = y
-    data = np.ones(m)
-    return csr_matrix((data, (row, col)), shape=(m, 10)).toarray()
+def get_y_matrix(y: np.ndarray, m: int) -> np.matrix:
+    """
+    Converts the given vector y to a matrix.
 
-# ==== OPGAVE 2c ==== 
-# ===== deel 1: =====
-def predict_number(Theta1, Theta2, X):
-    # Deze methode moet een matrix teruggeven met de output van het netwerk
-    # gegeven de waarden van Theta1 en Theta2. Elke regel in deze matrix 
-    # is de waarschijnlijkheid dat het sample op die positie (i) het getal
-    # is dat met de kolom correspondeert.
+    :param y: the vector to convert
+    :param m: the number of rows in the matrix
 
-    # De matrices Theta1 en Theta2 corresponderen met het gewicht tussen de
-    # input-laag en de verborgen laag, en tussen de verborgen laag en de
-    # output-laag, respectievelijk. 
-
-    # Een mogelijk stappenplan kan zijn:
-
-    #    1. voeg enen toe aan de gegeven matrix X; dit is de input-matrix a1
-    #    2. roep de sigmoid-functie van hierboven aan met a1 als actuele
-    #       parameter: dit is de variabele a2
-    #    3. voeg enen toe aan de matrix a2, dit is de input voor de laatste
-    #       laag in het netwerk
-    #    4. roep de sigmoid-functie aan op deze a2; dit is het uiteindelijke
-    #       resultaat: de output van het netwerk aan de buitenste laag.
-
-    # Voeg enen toe aan het begin van elke stap en reshape de uiteindelijke
-    # vector zodat deze dezelfde dimensionaliteit heeft als y in de exercise.
-
-    pass
+    :return: the matrix
+    """
+    y = y[:, 0] - 1  # Get only the first column and remove 1 from each value.
+    rows = np.arange(m)  # Create an array with values from 0 to m - 1.
+    data = np.ones(m)  # Create an array with m 1s.
+    return csr_matrix((data, (rows, y))).toarray()  # Create a sparse matrix
+    # with 1s at the correct
+    # positions.
 
 
+def predict_number(θ1: np.ndarray, θ2: np.ndarray, X: np.ndarray):
+    """
+    Returns a matrix with the output of the network given the values of
+    θ1 and θ2. Each row in this matrix is the probability that the sample
+    at that position (i) is the number that corresponds to the column.
 
-# ===== deel 2: =====
-def compute_cost(Theta1, Theta2, X, y):
-    # Deze methode maakt gebruik van de methode predictNumber() die je hierboven hebt
-    # geïmplementeerd. Hier wordt het voorspelde getal vergeleken met de werkelijk 
-    # waarde (die in de parameter y is meegegeven) en wordt de totale kost van deze
-    # voorspelling (dus met de huidige waarden van Theta1 en Theta2) berekend en
-    # geretourneerd.
-    # Let op: de y die hier binnenkomt is de m×1-vector met waarden van 1...10. 
-    # Maak gebruik van de methode get_y_matrix() die je in opgave 2a hebt gemaakt
-    # om deze om te zetten naar een matrix. 
+    The matrices θ1 and θ2 correspond to the weights between the input
+    layer and the hidden layer, and between the hidden layer and the
+    output layer, respectively.
 
-    pass
+    :param θ1: the weights between the input layer and the hidden layer
+    :param θ2: the weights between the hidden layer and the output layer
+    :param X: the input matrix
+
+    :return: the output of the network at the outer layer
+    """
+    a1 = np.c_[np.ones(X.shape[0]), X]  # Add ones to the input matrix.
+    a2 = sigmoid(a1 @ θ1.T)  # Calculate the output of the hidden layer.
+    a2 = np.c_[np.ones(a2.shape[0]), a2]  # Add ones to the hidden layer.
+    a3 = sigmoid(a2 @ θ2.T)  # Calculate the output of the outer layer.
+    return a3
 
 
+def compute_cost(θ1: np.ndarray, θ2: np.ndarray, X: np.ndarray, y: np.ndarray) -> float:
+    """
+    Computes the cost of the network given the values of θ1 and θ2 using
+    the given input matrix X and the given output vector y.
 
-# ==== OPGAVE 3a ====
-def sigmoid_gradient(z): 
-    # Retourneer hier de waarde van de afgeleide van de sigmoïdefunctie.
-    # Zie de opgave voor de exacte formule. Zorg ervoor dat deze werkt met
-    # scalaire waarden en met vectoren.
+    :param θ1: the weights between the input layer and the hidden layer
+    :param θ2: the weights between the hidden layer and the output layer
+    :param X: the input matrix
 
-    pass
+    :return: the cost of the network
+    """
+    m = X.shape[0]
+    y_matrix = get_y_matrix(y, m)  # Convert the output vector to a matrix.
+    # Calculate the output of the network.
+    J = predict_number(θ1, θ2, X)
+    cost = np.sum(-y_matrix * np.log(J) - (1 - y_matrix) * np.log(1 - J))  # ??
+    return cost / m  # Return the average cost.
 
-# ==== OPGAVE 3b ====
-def nn_check_gradients(Theta1, Theta2, X, y): 
-    # Retourneer de gradiënten van Theta1 en Theta2, gegeven de waarden van X en van y
-    # Zie het stappenplan in de opgaven voor een mogelijke uitwerking.
 
-    Delta2 = np.zeros(Theta1.shape)
-    Delta3 = np.zeros(Theta2.shape)
-    m = 1 #voorbeeldwaarde; dit moet je natuurlijk aanpassen naar de echte waarde van m
+def sigmoid_gradient(z: np.ndarray) -> np.ndarray:
+    """
+    Computes the gradient of the sigmoid function at z.
 
-    for i in range(m): 
-        #YOUR CODE HERE
-        pass
+    :param z: the input
 
-    Delta2_grad = Delta2 / m
-    Delta3_grad = Delta3 / m
-    
-    return Delta2_grad, Delta3_grad
+    :return: the gradient of the sigmoid function at z
+    """
+    return sigmoid(z) * (1 - sigmoid(z))
+
+
+def nn_check_gradients(θ1: np.ndarray, θ2: np.ndarray, X: np.ndarray, y: np.ndarray) -> tuple:
+    """
+    Backpropogates the network to calculate the gradients of the cost
+    function with respect to θ1 and θ2. Then checks whether these
+    gradients are correct by comparing them to the gradients calculated
+    using the finite difference method.
+
+    NOTE: really damn slow, don't panic if it takes a while.
+
+    :param θ1: the weights between the input layer and the hidden layer
+    :param θ2: the weights between the hidden layer and the output layer
+    :param X: the input matrix
+    :param y: the output vector
+
+    :return: a tuple containing the gradients of the cost function with
+    respect to θ1 and θ2
+    """
+    Δ2 = np.zeros(θ1.shape)
+    Δ3 = np.zeros(θ2.shape)
+    m: int = X.shape[0]
+
+    for i in range(m):
+        # === Step 1 ===
+        # predict_number(θ1, θ2, X[i, :])  # Calculate the output of the network.
+        a1 = np.c_[np.ones(X.shape[0]), X]  # Add ones to the input matrix.
+        z2 = a1 @ θ1.T
+        a2 = sigmoid(z2)  # Calculate the output of the hidden layer.
+        a2 = np.c_[np.ones(a2.shape[0]), a2]  # Add ones to the hidden layer.
+        z3 = a2 @ θ2.T
+        a3 = sigmoid(z3)  # Calculate the output of the outer layer.
+
+        # === Step 2 ===
+        # For each output unit k in layer 3 (the output layer), set
+        # δ3 = (a3 - y) where y is the expected output vector.
+        δ3 = a3 - y[i, :]
+
+        # === Step 3 ===
+        # For the hidden layer, set δ2 = Θ2T * δ3 .* g'(z2) where g'(z2)
+        # is the gradient of the sigmoid function at z2.
+        δ2 = (δ3 @ θ2)[:, 1:] * sigmoid_gradient(z2)
+
+        # === Step 4 ===
+        # Accumulate the gradient from this example using the following
+        # formula: Δ(l) = Δ(l) + δ(l+1) * a(l)T
+        Δ2 += δ2.T @ a1
+        Δ3 += δ3.T @ a2
+    # === Step 5 ===
+    # Obtain the unregularized gradient by dividing the accumulated
+    # gradients by m.
+
+    Δ2_grad = Δ2 / m
+    Δ3_grad = Δ3 / m
+
+    return Δ2_grad, Δ3_grad
